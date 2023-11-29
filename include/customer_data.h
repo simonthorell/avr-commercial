c0184c8c9a00abb8ba86ef7c060eb709b333f11b
NicoleNilsson <nicole-nilsson@outlook.com>
2023-11-29 17:41:58 +0100

    started on customer and message class

--- include/customer_data.h
+++ include/customer_data.h
@@ -1,4 +1,21 @@
 #ifndef CUSTOMER_DATA_H
 #define CUSTOMER_DATA_H
 
+class Customer {
+public:
+  unsigned int paid;
+  Message *list;
+};
+
+#define scrollFlag 1
+#define blinkFLag 2
+#define evenOnlyFlag 4
+#define oddOnlyFlag 8
+
+class Message { //make message struct or class?
+public:
+  char messageText[40]; //how to handle string in avr?
+  char flags;
+};
+
 #endif
