7ae4e57405c9dd8a517f3aad798988f5e5b71613
Cosmao <emil.kool@hotmail.com>
2023-11-29 18:22:00 +0100

    better looking å

--- src/main.cpp
+++ src/main.cpp
@@ -18,21 +18,20 @@ int main(void) {
   char text[] = "abc";
 
   // very good looking å
-  unsigned char customChar[8] = {0b00100, 0b01010, 0b00100, 0b01110,
-                                 0b00001, 0b01111, 0b10001, 0b01111};
-  //testing
-  unsigned char customCharTwo[8] = {
-    0b10000, 0b01000, 0b00100, 0b00010, 0b00001, 0b00010, 0b00100, 0b01000
-  };
+  unsigned char customChar[8] = {0b00100, 0b00000, 0b01110, 0b00001,
+                                 0b01111, 0b10001, 0b01111, 0b00000};
+  // testing
+  unsigned char customCharTwo[8] = {0b10000, 0b01000, 0b00100, 0b00010,
+                                    0b00001, 0b00010, 0b00100, 0b01000};
   lcd.CreateChar(0, customChar);
   lcd.CreateChar(1, customCharTwo);
   lcd.Clear();
   char test[5];
-  //Address for first custom character is 8 for some reason?
+  // Address for first custom character is 8 for some reason?
   test[0] = 0x8;
   test[1] = replaceÄ;
   test[2] = replaceÖ;
-  //second address is 9? Idk it should be 0 and 1
+  // second address is 9? Idk it should be 0 and 1
   test[3] = 0x9;
   test[4] = 0x0;
   lcd.WriteText(test);
