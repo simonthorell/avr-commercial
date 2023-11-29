e870447aae7e14362a100a9a0ec1dbd1b918dec7
Cosmao <emil.kool@hotmail.com>
2023-11-29 18:36:01 +0100

    Made macros for it now

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include "lcd.h"
 #include <avr/io.h>
 
-#define replaceÖ (char)0xEF
-#define replaceÄ (char)0xE1
+#define Ö "\xEF"
+#define Ä "\xE1"
+#define Å "\x08" //YOU NEED TO CREATE THE CHARACTER FIRST!
 
 int main(void) {
   // Create an instance of the LCD
@@ -14,9 +15,6 @@ int main(void) {
   // Clear the LCD screen
   lcd.Clear();
 
-  // Define the text to be displayed
-  char text[] = "abc";
-
   // very good looking å
   unsigned char customChar[8] = {0b00100, 0b00000, 0b01110, 0b00001,
                                  0b01111, 0b10001, 0b01111, 0b00000};
@@ -26,17 +24,9 @@ int main(void) {
   lcd.CreateChar(0, customChar);
   lcd.CreateChar(1, customCharTwo);
   lcd.Clear();
-  char test[5];
+  char test[] = Å Ä Ö " WOW!";
   // Address for first custom character is 8 for some reason?
-  test[0] = 0x8;
-  test[1] = replaceÄ;
-  test[2] = replaceÖ;
-  // second address is 9? Idk it should be 0 and 1
-  test[3] = 0x9;
-  test[4] = 0x0;
   lcd.WriteText(test);
-  lcd.GoTo(0, 1);
-  lcd.WriteText(text);
   // The microcontroller typically runs in an infinite loop
   while (1) {
     // Your main program logic here
