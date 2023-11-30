e3656f6286fcf06a15639288982ce14293e4ce68
Cosmao <emil.kool@hotmail.com>
2023-11-30 11:34:19 +0100

    removed redudnant main stuff

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,6 @@
 #define Ö "\xEF"
 #define Ä "\xE1"
 #define Å "\x08" //YOU NEED TO CREATE THE CHARACTER FIRST!
-#define customÅ { 0x4 , 0x0 , 0xe , 0x1 , 0xf , 0x11 , 0xf , 0x0}
 
 int main(void) {
   // Create an instance of the LCD
@@ -17,11 +16,6 @@ int main(void) {
   lcd.Clear();
 
   // testing
-  unsigned char customCharTwo[8] = {0b10000, 0b01000, 0b00100, 0b00010,
-                                    0b00001, 0b00010, 0b00100, 0b01000};
-  unsigned char testArray[] = customÅ;
-  lcd.CreateChar(0, testArray);
-  lcd.CreateChar(1, customCharTwo);
   lcd.Clear();
   char test[] = "En god bilaff" Ä "r (f" Ö "r Harry!)";
   // Address for first custom character is 8 for some reason?
