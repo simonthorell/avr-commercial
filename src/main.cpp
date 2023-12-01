349468413d97f8c8a1c0264d22eaf62ffae53c11
Cosmao <emil.kool@hotmail.com>
2023-12-01 14:22:44 +0100

    removed double define

--- src/main.cpp
+++ src/main.cpp
@@ -7,14 +7,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-//Only here for debug purposes, lets you print out the binary format
-#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
-#define BYTE_TO_BINARY(byte)                                                   \
-  ((byte) & 0x80 ? '1' : '0'), ((byte) & 0x40 ? '1' : '0'),                    \
-      ((byte) & 0x20 ? '1' : '0'), ((byte) & 0x10 ? '1' : '0'),                \
-      ((byte) & 0x08 ? '1' : '0'), ((byte) & 0x04 ? '1' : '0'),                \
-      ((byte) & 0x02 ? '1' : '0'), ((byte) & 0x01 ? '1' : '0')
-
 int main(void) {
   // Create an instance of the LCD
   HD44780 lcd;
