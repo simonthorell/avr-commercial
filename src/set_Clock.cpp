5437192676a714090ad642551c680e384b29791a
Cosmao <emil.kool@hotmail.com>
2023-12-05 16:18:10 +0100

    timer working

--- src/set_Clock.cpp
+++ src/set_Clock.cpp
@@ -7,48 +7,45 @@ const int buttonSetPin = PB3;    // Pin for the set button
 
 int setClock(HD44780 *lcd) {
 
-  initializeButtons();
+  // initializeButtons();
+  char time[12];
+  uint8_t currentSeconds = seconds;
 
   while (1) {
 
-  uint8_t currentSeconds = seconds;
-    char time[12];
+    currentSeconds = seconds;
     sprintf(time, "%02d:%02d:%02d", hours, minutes, seconds);
     displayText(lcd, time);
-    while (currentSeconds == seconds) {
 
+    while (currentSeconds == seconds) {
       if (readButton(buttonHourPin)) {
-        _delay_ms(50);  // Debounce delay
-        hours = (hours % 24) + 1;  // Increment hours from 1 to 24
-        if(hours > 24){
+        _delay_ms(50);            // Debounce delay
+        hours = (hours % 24) + 1; // Increment hours from 1 to 24
+        if (hours > 24) {
           hours = 0;
         }
       }
-
       // Button to iterate through minutes
       if (readButton(buttonMinutePin)) {
         _delay_ms(50);
-        minutes = (minutes + 1) % 60;  // Increment minutes from 0 to 59
-        if(minutes > 60){
+        minutes = (minutes + 1) % 60; // Increment minutes from 0 to 59
+        if (minutes > 60) {
           minutes = 0;
         }
       }
-
       // Button to iterate through seconds
       if (readButton(buttonSecondPin)) {
         _delay_ms(50);
-        seconds = (seconds + 1) % 60;  // Increment seconds from 0 to 59
-        if(seconds > 60){
+        seconds = (seconds + 1) % 60; // Increment seconds from 0 to 59
+        if (seconds > 60) {
           seconds = 0;
         }
       }
-
       // Button to set the time
       if (readButton(buttonSetPin)) {
-        _delay_ms(50); 
+        _delay_ms(50);
         break;
       }
-
     }
 
   }
@@ -57,10 +54,10 @@ int setClock(HD44780 *lcd) {
 }
 
 void initializeButtons() {
-  DDRD &= ~((1 << buttonHourPin) | (1 << buttonMinutePin) | (1 << buttonSecondPin) | (1 << buttonSetPin));
-  PORTB |= (1 << buttonHourPin) | (1 << buttonMinutePin) | (1 << buttonSecondPin) | (1 << buttonSetPin);
+  DDRD &= ~((1 << buttonHourPin) | (1 << buttonMinutePin) |
+            (1 << buttonSecondPin) | (1 << buttonSetPin));
+  PORTB |= (1 << buttonHourPin) | (1 << buttonMinutePin) |
+           (1 << buttonSecondPin) | (1 << buttonSetPin);
 }
 
-int readButton(uint8_t pin) {
-  return bit_is_clear(PIND, pin);
-}
+int readButton(uint8_t pin) { return !bit_is_clear(PIND, pin); }
