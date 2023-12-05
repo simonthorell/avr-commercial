c9dcf412e14587b55a4d2db29a6ddae4486be277
Disce-et-facere <krantzr@hotmail.se>
2023-12-05 16:56:32 +0100

    modified:   src/set_Clock.cpp almost there...

--- src/set_Clock.cpp
+++ src/set_Clock.cpp
@@ -9,50 +9,52 @@ int setClock(HD44780 *lcd) {
 
   initializeButtons();
 
+  uint8_t currentSeconds = seconds;
+  char time[12];
+
   while (1) {
 
-  uint8_t currentSeconds = seconds;
-    char time[12];
+    
     sprintf(time, "%02d:%02d:%02d", hours, minutes, seconds);
     displayText(lcd, time);
 
     while (currentSeconds == seconds) {
       if (readButton(buttonHourPin)) {
-        _delay_ms(50);  // Debounce delay
+        _delay_ms(250);  // Debounce delay
         hours = (hours % 24) + 1;  // Increment hours from 1 to 24
-        if(hours > 24){
+        /*if(hours > 24){
           hours = 0;
-        }
+        }*/
         sprintf(time, "%02d:%02d:%02d", hours, minutes, seconds);
         displayText(lcd, time);
         break;
       }
       // Button to iterate through minutes
       if (readButton(buttonMinutePin)) {
-        _delay_ms(50);
+        _delay_ms(250);
         minutes = (minutes + 1) % 60;  // Increment minutes from 0 to 59
-        if(minutes > 60){
+        /*if(minutes > 60){
           minutes = 0;
-        }
+        }*/
         sprintf(time, "%02d:%02d:%02d", hours, minutes, seconds);
         displayText(lcd, time);
         break;
       }
       // Button to iterate through seconds
       if (readButton(buttonSecondPin)) {
-        _delay_ms(50);
+        _delay_ms(250);
         seconds = (seconds + 1) % 60;  // Increment seconds from 0 to 59
-        if(seconds > 60){
+        /*if(seconds > 60){
           seconds = 0;
-        }
+        }*/
         sprintf(time, "%02d:%02d:%02d", hours, minutes, seconds);
         displayText(lcd, time);
         break;
       }
       // Button to set the time
       if (readButton(buttonSetPin)) {
-        _delay_ms(50); 
-        break;
+        _delay_ms(100); 
+        return 0;
       }
 
     }
