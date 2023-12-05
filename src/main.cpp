91f459a123a6d12b85f84e97092113ab9a8331f1
Disce-et-facere <krantzr@hotmail.se>
2023-12-05 16:37:35 +0100

    modified:   src/main.cpp 	modified:   src/set_Clock.cpp almost there

--- src/main.cpp
+++ src/main.cpp
@@ -15,7 +15,7 @@ void displayMessage(uint8_t winner, HD44780 *lcd, pseudoRandom *rnd);
 
 int main(void) {
   // initialize timer (TODO: How to sync with NTP-server?)
-  timer_init();
+  
   
   // Create an instance of the LCD and random
   HD44780 lcd;
@@ -33,7 +33,7 @@ int main(void) {
   lcd.Clear();
 
   setClock(&lcd);
-
+  timer_init();
   uint8_t lastShown = maxCustomers; // Out of bounds to start
   uint8_t winningCustomer = rnd.getRandomCustomer(maxCustomers, totalPayed);
 
--- src/set_Clock.cpp
+++ src/set_Clock.cpp
@@ -7,48 +7,71 @@ const int buttonSetPin = PB3;    // Pin for the set button
 
 int setClock(HD44780 *lcd) {
 
-  initializeButtons();
-
-  while (1) {
-
+  
+  bool done = false;
+  char time[12];
   uint8_t currentSeconds = seconds;
-    char time[12];
-    sprintf(time, "%02d:%02d:%02d", hours, minutes, seconds);
+  int hour = 0;
+  int minute = 0;
+  int second = 0;
+
+  sprintf(time, "%02d:%02d:%02d", hours, minutes, seconds);
+  displayText(lcd, time);
+  initializeButtons();
+  while (1) { 
+    
+    
+
     displayText(lcd, time);
+
     while (currentSeconds == seconds) {
 
       if (readButton(buttonHourPin)) {
-        _delay_ms(50);  // Debounce delay
+        _delay_ms(100);  // Debounce delay
         hours = (hours % 24) + 1;  // Increment hours from 1 to 24
         if(hours > 24){
           hours = 0;
         }
+        sprintf(time, "%02d:%02d:%02d", hours, minutes, seconds);
+        displayText(lcd, time);
+        break;
       }
 
       // Button to iterate through minutes
       if (readButton(buttonMinutePin)) {
-        _delay_ms(50);
+        _delay_ms(100);
         minutes = (minutes + 1) % 60;  // Increment minutes from 0 to 59
         if(minutes > 60){
           minutes = 0;
         }
+        sprintf(time, "%02d:%02d:%02d", hours, minutes, seconds);
+        displayText(lcd, time);
+        break;
       }
 
       // Button to iterate through seconds
       if (readButton(buttonSecondPin)) {
-        _delay_ms(50);
+        _delay_ms(100);
         seconds = (seconds + 1) % 60;  // Increment seconds from 0 to 59
         if(seconds > 60){
           seconds = 0;
         }
+        sprintf(time, "%02d:%02d:%02d", hours, minutes, seconds);
+        displayText(lcd, time);
+        break;
       }
 
       // Button to set the time
       if (readButton(buttonSetPin)) {
         _delay_ms(50); 
-        break;
+        done = true;
       }
+     
 
+    }
+
+    if(done){
+    
     }
 
   }
@@ -57,10 +80,10 @@ int setClock(HD44780 *lcd) {
 }
 
 void initializeButtons() {
-  DDRD &= ~((1 << buttonHourPin) | (1 << buttonMinutePin) | (1 << buttonSecondPin) | (1 << buttonSetPin));
+  DDRB &= ~((1 << buttonHourPin) | (1 << buttonMinutePin) | (1 << buttonSecondPin) | (1 << buttonSetPin));
   PORTB |= (1 << buttonHourPin) | (1 << buttonMinutePin) | (1 << buttonSecondPin) | (1 << buttonSetPin);
 }
 
 int readButton(uint8_t pin) {
-  return bit_is_clear(PIND, pin);
+  return bit_is_clear(PINB, pin);
 }
