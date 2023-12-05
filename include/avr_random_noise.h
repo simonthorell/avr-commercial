51e0f63e343782f64020f534a8e0abed45e71962
Cosmao <emil.kool@hotmail.com>
2023-12-05 10:56:36 +0100

    removed debug code

--- include/avr_random_noise.h
+++ include/avr_random_noise.h
@@ -2,18 +2,8 @@
 #define __avr_random_noise_h
 
 #include "customer_data.h"
-#include "lcd.h" //FIXME: Remove when randomTest is gone
 #include <avr/io.h>
 #include <stdint.h> //So I dont get all the uint errors, just for my IDE
-
-// Only here for debug purposes, lets you print out the binary format
-// FIXME: Remove when randomTest is gone
-#define BYTE_TO_BINARY_PATTERN "%c%c%c%c%c%c%c%c"
-#define BYTE_TO_BINARY(byte)                                                   \
-  ((byte) & 0x80 ? '1' : '0'), ((byte) & 0x40 ? '1' : '0'),                    \
-      ((byte) & 0x20 ? '1' : '0'), ((byte) & 0x10 ? '1' : '0'),                \
-      ((byte) & 0x08 ? '1' : '0'), ((byte) & 0x04 ? '1' : '0'),                \
-      ((byte) & 0x02 ? '1' : '0'), ((byte) & 0x01 ? '1' : '0')
 
 class pseudoRandom {
 public:
@@ -22,7 +12,6 @@ public:
   ~pseudoRandom(void);
   uint16_t getRandom(uint16_t maxInclusive);
   uint16_t getRandom(uint16_t minInclusive, uint16_t maxInclusive);
-  void randomTest(HD44780 *lcd); // FIXME: Remove before release
   uint8_t getRandomCustomer(uint8_t maxCustomers, uint16_t totalPayed);
   uint8_t getRandomCustomer(uint8_t maxCustomers, uint16_t totalPayed,
                             HD44780 *lcd);
--- src/avr_random_noise.cpp
+++ src/avr_random_noise.cpp
@@ -1,5 +1,4 @@
 #include "avr_random_noise.h"
-#include "lcd.h" //FIXME: Rmove with randomTest
 #include <avr/io.h>
 #include <stdint.h> //So I dont get all the uint errors, just for my IDE
 #include <stdio.h>
@@ -77,29 +76,6 @@ uint16_t pseudoRandom::scaleNumber(uint16_t minInclusive, uint16_t maxInclusive,
   return (uint16_t)dvalue;
 }
 
-// FIXME: Remove before release
-void pseudoRandom::randomTest(HD44780 *lcd) {
-  uint16_t rnd = 0;
-  uint8_t rndLow = 0;
-  uint8_t rndHigh = 0;
-  char rndBuff[30];
-  while (1) {
-    lcd->Clear();
-    lcd->GoTo(0, 0);
-    rnd = getRandom(2, 15235);
-    rndLow = rnd & 0xff;
-    rndHigh = rnd >> 8;
-    sprintf(rndBuff, "%u", rnd);
-    lcd->WriteText(rndBuff);
-    sprintf(rndBuff, BYTE_TO_BINARY_PATTERN BYTE_TO_BINARY_PATTERN,
-            BYTE_TO_BINARY(rndHigh), BYTE_TO_BINARY(rndLow));
-    lcd->GoTo(0, 1);
-    lcd->WriteText(rndBuff);
-    _delay_ms(500);
-  }
-  return;
-}
-
 uint8_t pseudoRandom::getRandomCustomer(uint8_t maxCustomers,
                                         uint16_t totalPayed) {
   uint16_t random = getRandom(0, totalPayed);
@@ -116,25 +92,12 @@ uint8_t pseudoRandom::getRandomCustomer(uint8_t maxCustomers,
 uint8_t pseudoRandom::getRandomCustomer(uint8_t maxCustomers,
                                         uint16_t totalPayed, HD44780 *lcd) {
   uint16_t random = getRandom(0, totalPayed);
-  // FIXME: Remove before release
-  char buff[30];
-  lcd->Clear();
-  sprintf(buff, "%u %u %u", random, totalPayed, maxCustomers);
-  lcd->WriteText(buff);
-  _delay_ms(2000);
-  // FIXME_END
   for (uint8_t i = 0; i < maxCustomers; i++) {
     Customer customer = getCustomer(i);
     if (random < customer.balance) {
       return i;
     }
     random -= customer.balance;
-    // FIXME: Remove before release
-    lcd->Clear();
-    sprintf(buff, "%u %u", random, i);
-    lcd->WriteText(buff);
-    _delay_ms(1000);
-    // FIXME_END
   }
   return 0;
 }
