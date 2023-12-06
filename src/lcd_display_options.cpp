a53f941c5a3b3ef7ff6593cae67e0a805e75b90c
Cosmao <emil.kool@hotmail.com>
2023-12-06 13:03:51 +0100

    scroll thingy working

--- src/lcd_display_options.cpp
+++ src/lcd_display_options.cpp
@@ -1,113 +1,126 @@
+#include "lcd_display_options.h"
+#include "customer_data.h"
+#include "lcd.h"
+#include "timer.h" // Minutes and seconds
 #include <avr/io.h>
 #include <util/delay.h>
-#include "lcd.h"
-#include "lcd_display_options.h"
-#include "timer.h" // Minutes and seconds
 
 #define DISPLAY_TIME 20000
 
 /********************************************************************
-*                   LCD DISPLAY BILLBOARD MAIN FUNCTION
-********************************************************************/
+ *                   LCD DISPLAY BILLBOARD MAIN FUNCTION
+ ********************************************************************/
 
-uint8_t displayBillboard(HD44780 *lcd, char* text, int length, char displayProperties) {
-    uint8_t timeStatus = currentTime();
+uint8_t displayBillboard(HD44780 *lcd, char *text, int length,
+                         char displayProperties) {
+  uint8_t timeStatus = currentTime();
 
-    // Check TIME_MIN_EVEN flag
-    if ((displayProperties & TIME_MIN_EVEN) && (timeStatus != EVEN_MINUTE)) {
-        return TIME_ERROR;
-    }
+  // Check TIME_MIN_EVEN flag
+  if ((displayProperties & TIME_MIN_EVEN) && (timeStatus != EVEN_MINUTE)) {
+    return TIME_ERROR;
+  }
 
-    // Check TIME_MIN_ODD flag
-    if ((displayProperties & TIME_MIN_ODD) && (timeStatus != ODD_MINUTE)) {
-        return TIME_ERROR;
-    }
+  // Check TIME_MIN_ODD flag
+  if ((displayProperties & TIME_MIN_ODD) && (timeStatus != ODD_MINUTE)) {
+    return TIME_ERROR;
+  }
 
-    // Check STATIC flag
-    if (displayProperties & STATIC) {
-        displayStaticText(lcd, text);
-        return SUCCESS;
-    }
+  // Check STATIC flag
+  if (displayProperties & STATIC) {
+    displayStaticText(lcd, text);
+    return SUCCESS;
+  }
 
-    // Check BLINKING flag
-    if (displayProperties & BLINKING) {
-        displayBlinkingText(lcd, text);
-        return SUCCESS;
-    }
+  // Check BLINKING flag
+  if (displayProperties & BLINKING) {
+    displayBlinkingText(lcd, text);
+    return SUCCESS;
+  }
 
-    // Check SCROLLING flag
-    if (displayProperties & SCROLLING) {
-        displayScrollingText(lcd, text, length);
-        return SUCCESS;
-    }
+  // Check SCROLLING flag
+  if (displayProperties & SCROLLING) {
+    displayScrollingText(lcd, text, length);
+    return SUCCESS;
+  }
 
-    /*** TESTING BITWISE ***/
-    // displayProperties = TIME_MIN_EVEN | BLINKING;
-    // char binaryStr[9];  // Buffer for binary string representation
-    // for (int i = 7; i >= 0; --i) {
-    //     binaryStr[7 - i] = (displayProperties & (1 << i)) ? '1' : '0';
-    // }
-    // binaryStr[8] = '\0';  // Null-terminate the string
-    // displayStaticText(lcd, binaryStr);
+  /*** TESTING BITWISE ***/
+  // displayProperties = TIME_MIN_EVEN | BLINKING;
+  // char binaryStr[9];  // Buffer for binary string representation
+  // for (int i = 7; i >= 0; --i) {
+  //     binaryStr[7 - i] = (displayProperties & (1 << i)) ? '1' : '0';
+  // }
+  // binaryStr[8] = '\0';  // Null-terminate the string
+  // displayStaticText(lcd, binaryStr);
 
-    return NOT_FOUND_ERROR;
+  return NOT_FOUND_ERROR;
 }
 
 /********************************************************************
-*                        LCD DISPLAY OPTIONS
-********************************************************************/
+ *                        LCD DISPLAY OPTIONS
+ ********************************************************************/
 
-void displayStaticText(HD44780 *lcd, char* text) {
+void displayStaticText(HD44780 *lcd, char *text) {
+  displayText(lcd, text);
+  _delay_ms(DISPLAY_TIME);
+}
+
+void displayScrollingText(HD44780 *lcd, char *text, int length) {
+  for (int i = length; i < 45; i++) {
+    text[i] = ' ';
+  }
+  text[MAX_CUSTOMER_NAME_LENGTH - 1] = '\0';
+  int duration = DISPLAY_TIME;
+  while (duration > 0) {
+    for (int i = 0; i < length; i++) {
+      displayText(lcd, text);
+      _delay_ms(200);
+      duration -= 200;
+      if (duration <= 0) {
+        return;
+      }
+      scrollText(text, length);
+    }
+  }
+}
+
+void displayBlinkingText(HD44780 *lcd, char *text) {
+  int duration = DISPLAY_TIME;
+  while (duration > 0) {
     displayText(lcd, text);
-    _delay_ms(DISPLAY_TIME);
-}
-
-void displayScrollingText(HD44780 *lcd, char* text, int length) {
-    int duration = DISPLAY_TIME;
-    while(duration > 0){
-        for(int i = 0; i < length; i++) {
-            displayText(lcd, text);
-            _delay_ms(200);
-            duration -= 200;
-            if (duration <= 0) {
-                return;
-            }
-            scrollText(text, length);
-        }
-    }
-}
-
-void displayBlinkingText(HD44780 *lcd, char* text) {
-    int duration = DISPLAY_TIME;
-    while(duration > 0) {
-        displayText(lcd, text);
-        _delay_ms(500);
-        lcd->Clear();
-        _delay_ms(500);
-        duration -= 1000;
-    }
+    _delay_ms(500);
+    lcd->Clear();
+    _delay_ms(500);
+    duration -= 1000;
+  }
 }
 
 /********************************************************************
-*                         HELPER FUNCTIONS
-********************************************************************/
+ *                         HELPER FUNCTIONS
+ ********************************************************************/
 
-void scrollText(char* text, int length) {
-    char first = text[0];
-    for(int i = 0; i < length - 1; i++) {
-        text[i] = text[i + 1];
+void scrollText(char *text, int length) {
+ length = length < 32 ? 32 : length;
+  char first = text[0];
+  for (int i = 0; i < length - 1; i++) {
+    text[i] = text[i + 1];
+  }
+  for(int i = 0; i < length; i++){
+    if(text[i] == '\0'){
+      text[i] = ' ';
     }
-    text[length - 1] = first;
+  }
+  text[length - 1] = first;
+  text[length] = '\0';
 }
 
-void displayText(HD44780 *lcd, char* text) {
-    lcd->Clear();
-    lcd->WriteText(text);
+void displayText(HD44780 *lcd, char *text) {
+  lcd->Clear();
+  lcd->WriteText(text);
 }
 
-uint8_t getStrLen(char *text){
+uint8_t getStrLen(char *text) {
   uint8_t len = 0;
-  while(*(text + len) != 0 ){
+  while (*(text + len) != 0) {
     len++;
   }
   len++;
