c73291838587473d56f3e6ead02406863031e9a3
Simon Thorell <kontakt@simonthorell.se>
2023-12-07 19:41:04 +0100

    Refactored main for better testing and read-ability

--- src/main.cpp
+++ src/main.cpp
@@ -1,77 +1,100 @@
-#include "avr_random_noise.h"
-#include "customer_data.h"
-#include "lcd.h"
-#include "lcd_display_options.h"
-#include "set_Clock.h"
+#include "avr_random_noise.h"    // Generate random numbers
+#include "customer_data.h"       // Customer data
+#include "lcd.h"                 // LCD class
+#include "lcd_display_options.h" // Display billboards
+#include "set_Clock.h"           // Set start time
 #include "timer.h"               // Start the timer/clock
 #include "buttons.h"             // Button interrupts
 #include "special_functions.h"   // Button interrupts
-#include <avr/io.h>
-#include <stdint.h>  // So I dont get all the uint errors, just for my IDE
-#include <stdlib.h>
-#include <util/delay.h>
+#include <avr/io.h>              // For ISR
+#include <stdint.h>              // uint8_t (NeoVIM)
+#include <stdlib.h>              // std return values
 
-void displayMessage(uint8_t winner, HD44780 *lcd, pseudoRandom *rnd);
+// Global variables alive throughout the program
+HD44780 lcd;                     // Create an instance of the LCD
+pseudoRandom rnd;                // Create an instance of random
+
+// Function declarations
+int runBillboards();
+int calculateTotalPayed(uint16_t *totalPayed);
+int checkLastCustomer(uint8_t &lastShown, uint8_t &winningCustomer, uint16_t totalPayed);
+int displayMessage(uint8_t winner, HD44780 *lcd, pseudoRandom *rnd);
+
+/********************************************************************
+ *                          MAIN / SETUP
+ ********************************************************************/
 
 int main(void) {
-  // Create an instance of the LCD and random
-  HD44780 lcd;
-  pseudoRandom rnd;
+  lcd.Initialize();   // Initialize the LCD
+  lcd.Clear();        // Clear the LCD
 
-  uint16_t totalPayed = 0;
-  for (uint8_t i = 0; i < getNumCustomers(); i++) {
-    Customer customer = getCustomer(i);
-    totalPayed += customer.balance;
-  }
+  setClock(&lcd);     // Set start time
+  timer_init();       // Start timer/clock
+  
+  initializeButtons();            // Configure buttons as inputs
+  initializeButtonInterrupts();   // Enable interrupts
 
-  // Initialize & clear the LCD
-  lcd.Initialize();
-  lcd.Clear();
-
-  // Set start time and start timer
-  timer_init();
-  setClock(&lcd);
-
-  // Initialize buttons interupts for 'special functions'
-  initializeButtons();
-  initializeButtonInterrupts();
-
-  uint8_t lastShown = getNumCustomers(); // Out of bounds to start
-  uint8_t winningCustomer =
-      rnd.getRandomCustomer(getNumCustomers(), totalPayed);
-
-  while (1) {
-    // Making sure same customer dont get shown twice
-    while (lastShown == winningCustomer) {
-      winningCustomer = rnd.getRandomCustomer(getNumCustomers(), totalPayed);
-    }
-    lastShown = winningCustomer;
-
-    displayMessage(winningCustomer, &lcd, &rnd);
-  }
+  runBillboards(); // Start the loop
+  
   return EXIT_SUCCESS;
 }
 
-// Display random billboard
-void displayMessage(uint8_t winner, HD44780 *lcd, pseudoRandom *rnd) {
-  char buff[MAX_BILLBOARD_TEXT_LENGTH]; // some extra padding
+/********************************************************************
+ *                      APPLICATION CORE LOOP
+ ********************************************************************/
+
+int runBillboards() {
+  uint16_t totalPayed = 0;
+  calculateTotalPayed(&totalPayed);
+
+  uint8_t lastShown = 255; // Out of bounds to start (max uint8_t = 255)
+  uint8_t winningCustomer = rnd.getRandomCustomer(getNumCustomers(), totalPayed);
+
+  while (1) {
+    checkLastCustomer(lastShown, winningCustomer, totalPayed);
+    displayMessage(winningCustomer, &lcd, &rnd);
+  }
+
+  return SUCCESS;
+}
+
+/********************************************************************
+ *                         HELPER FUNCTIONS
+ ********************************************************************/
+
+int calculateTotalPayed(uint16_t *totalPayed) {
+  for (uint8_t i = 0; i < getNumCustomers(); i++) {
+    Customer customer = getCustomer(i);
+    *totalPayed += customer.balance;
+  }
+  return SUCCESS;
+}
+
+int checkLastCustomer(uint8_t &lastShown, uint8_t &winningCustomer, uint16_t totalPayed) {
+  while (lastShown == winningCustomer) {
+    winningCustomer = rnd.getRandomCustomer(getNumCustomers(), totalPayed);
+  }
+  lastShown = winningCustomer;
+  return SUCCESS;
+}
+
+int displayMessage(uint8_t winner, HD44780 *lcd, pseudoRandom *rnd) {
+  char buff[MAX_BILLBOARD_TEXT_LENGTH];
   Customer customer = getCustomer(winner);
 
-  uint8_t billboardDisplayed =
-      -1; // Out of bounds to start to not randomly get assigned 0
+  uint8_t billboardDisplayed = -1;
   while (billboardDisplayed != SUCCESS) {
-    uint8_t lastBillboard = -1; // Out of bounds to start
+    static uint8_t lastBillboard = -1;
     uint8_t randomBillboard = rnd->getRandom(customer.billboardsCount);
 
     if (randomBillboard != lastBillboard) {
-      message custMessage =
-          getMessage(&customer, rnd->getRandom(customer.billboardsCount - 1));
+      message custMessage = getMessage(&customer, rnd->getRandom(customer.billboardsCount - 1));
       memcpy_P(&buff, custMessage.messageText, 40);
-      billboardDisplayed = displayBillboard(lcd, buff, getStrLen(buff),
-                                            custMessage.messageFlags);
+
+      billboardDisplayed = displayBillboard(lcd, buff, getStrLen(buff), custMessage.messageFlags);
     }
     lastBillboard = randomBillboard;
   }
 
-  return;
+  return SUCCESS;
 }
